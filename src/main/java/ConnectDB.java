6e18fd74fd6c1c19efd655d6fc0c76fc8bcae708
trungHacker2k3 <ndtrung30@gmail.com>
2023-08-25 11:29:28 +0700

    connect mysql

---
+++ src/main/java/ConnectDB.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectDB {
+	public ConnectDB() {};
+	
+	private String user = "root";
+	private String pass = "";
+	
+	public Connection getConnection() {
+		Connection conn = null;
+		try {
+			String dbName = "";
+			String url = "jdbc:mysql://localhost/" + dbName;
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(url, user, pass);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+}
